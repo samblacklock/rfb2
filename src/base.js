eb0fa201d6049320d93bc4fd183224e45624bea0
samblacklock <samblacklock@me.com>
2017-04-18 10:51:08 +0100

    latest:

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDXZgL7ytv0Oe7ajjfVo_XafoPj9HumHFs",
+  authDomain: "rfb2-6872f.firebaseapp.com",
+  databaseURL: "https://rfb2-6872f.firebaseio.com"
+});
+
+export default base;
